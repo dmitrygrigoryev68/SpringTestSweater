4915aff243072cb89738dc23f9c11a7947bf4f0f
Dmitry Grigoryev <dmitry.grigoryev@gmx.de>
2019-01-14 17:46:19 +0100

    first commit

---
+++ src/main/java/com/example/sweater/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.sweater;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World")
+           String name, Map<String, Object> model) {
+               model.put("name", name);
+               return "hello";
+    }
+
+}
